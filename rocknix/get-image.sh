d6f122c8241d63f93a3b8a4ee35f77423aa04096
EatPrilosec <ae3nerdgod@gmail.com>
2025-05-31 19:05:54 -0400

    Update get-image.sh

--- rocknix/get-image.sh
+++ rocknix/get-image.sh
@@ -7,6 +7,6 @@ then
     [[ -f "${ThisimgName}" ]] && rm ${ThisimgName}
     ROCKNIXdl=$(lynx -listonly -dump -nonumbers "https://github.com/ROCKNIX/distribution/releases/latest" |grep "ROCKNIX-RK3326.aarch64-.*-a.img.gz" 2>/dev/null)
     echo $ROCKNIXdl
-    wget -O${ThisimgName}.gz "$ROCKNIXdl"
+    wget  --no-verbose --quiet -O${ThisimgName}.gz "$ROCKNIXdl"
     gunzip -f ${ThisimgName}.gz
 fi
