d7498ef4bf2e0ad176ddfbc74850c7c37174a977
EatPrilosec <ae3nerdgod@gmail.com>
2025-05-31 19:05:36 -0400

    Update get-image.sh

--- armbian/get-image.sh
+++ armbian/get-image.sh
@@ -4,6 +4,6 @@ if [[ ! -f "armbian.img" ]]
 then
     [[ -f "armbian.img.xz" ]] && rm armbian.img.xz
     armbiandl=https://dl.armbian.com/uefi-arm64/Noble_current_xfce
-    wget --no-check-certificate -Oarmbian.img.xz "$armbiandl"
+    wget  --no-verbose --quiet --no-check-certificate -Oarmbian.img.xz "$armbiandl"
     unxz --force --decompress armbian.img.xz
 fi
