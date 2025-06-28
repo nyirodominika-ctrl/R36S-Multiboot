8c5f711c12661995661ef8977b25b2ebdbb21eec
EatPrilosec <munch@DESKTOP-GPFUGIC>
2025-06-28 09:26:14 -0400

    add all page

--- amberelec/install-os.sh
+++ amberelec/install-os.sh
@@ -7,16 +7,16 @@ sayin new $((npsz/1024))GiB partition
 newpart $npsz ext4 $OsName
 
 InstallDev=${ImgLodev}p${partcount}
-sayin new dev is ${InstallDev} 
+sayin new dev is ${InstallDev}
 
 sayin setup mounts
 ThisBootMnt=${tmpmnts}/boot
-mkdir -p "${ThisBootMnt}" 
+mkdir -p "${ThisBootMnt}"
 
 Thislodev=$(losetup -f)
 sudo losetup -P ${Thislodev} ${ThisImgName}
 sudo mount ${Thislodev}p1 "${ThisBootMnt}"
-sudo mkdir -p "${ImgBootMnt}/${OsName}" 
+sudo mkdir -p "${ImgBootMnt}/${OsName}"
 
 sayin copy boot files to ${imgname}
 
@@ -31,7 +31,7 @@ do
 done
 
 sayin copy KERNEL and SYSTEM to ${imgname} boot partition
-sudo cp -v "${ThisBootMnt}/KERNEL" "${ImgBootMnt}/${OsName}" 
-sudo cp -v "${ThisBootMnt}/KERNEL.md5" "${ImgBootMnt}/${OsName}" 
-sudo cp -v "${ThisBootMnt}/SYSTEM" "${ImgBootMnt}/${OsName}" 
-sudo cp -v "${ThisBootMnt}/SYSTEM.md5" "${ImgBootMnt}/${OsName}" 
+sudo cp -v "${ThisBootMnt}/KERNEL" "${ImgBootMnt}/${OsName}"
+sudo cp -v "${ThisBootMnt}/KERNEL.md5" "${ImgBootMnt}/${OsName}"
+sudo cp -v "${ThisBootMnt}/SYSTEM" "${ImgBootMnt}/${OsName}"
+sudo cp -v "${ThisBootMnt}/SYSTEM.md5" "${ImgBootMnt}/${OsName}"
